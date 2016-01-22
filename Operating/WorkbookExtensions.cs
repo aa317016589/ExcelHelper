05a2e5373efb1fe110a9194e17a33c3f84e699d1
aa317016589 <317016589@qq.com>
2016-01-22 21:28:04 +0800

    修复IE下载excel的时候文件名乱码问题。

--- Operating/WorkbookExtensions.cs
+++ Operating/WorkbookExtensions.cs
@@ -3,6 +3,7 @@ using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using System.IO;
 using System.Web;
+using System.Text;
 
 namespace ExcelHelper.Operating
 {
@@ -12,40 +13,40 @@ namespace ExcelHelper.Operating
         {
             string contentType = string.Empty;
 
-            if (workbook.GetType() == typeof(HSSFWorkbook))
+            if (workbook.GetType() == typeof (HSSFWorkbook))
             {
                 //Office2003
                 contentType = "application/vnd.ms-excel";
             }
-            else if ((workbook.GetType() == typeof(XSSFWorkbook)))
+            else if ((workbook.GetType() == typeof (XSSFWorkbook)))
             {
                 //Office2007
                 contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
             }
 
             var request = HttpContext.Current.Request;
-            if (request.UserAgent.ToLower().IndexOf("msie") > -1)
-            {
-                fileName = System.Web.HttpUtility.UrlEncode(fileName, System.Text.Encoding.UTF8);
-            }
-            else if (request.UserAgent.ToLower().IndexOf("firefox") > -1)
+
+            if (request.UserAgent.ToLower().IndexOf("firefox") > -1)
             {
                 fileName = "\"" + fileName + "\"";
             }
+
+            fileName = HttpUtility.UrlEncode(fileName, Encoding.UTF8);
+
 
             var response = HttpContext.Current.Response;
             response.Clear();
             response.ContentType = contentType;
             response.Charset = "uft-8";
             response.ContentEncoding = System.Text.Encoding.UTF8;
-            response.AddHeader("Content-Disposition", string.Format("attachment;filename={0}",fileName));
+            response.AppendHeader("Content-Disposition", string.Format("attachment;filename={0}", fileName));
 
             using (MemoryStream ms = new MemoryStream())
             {
                 workbook.Write(ms);
                 ms.WriteTo(response.OutputStream);
             }
-            
+
             response.Flush();
             response.End();
 
